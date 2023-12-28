3c8aea6fdb76fccfd5c68d260eecb2cf75a694f6
Jang-GO <jang8195@naver.com>
2023-12-28 23:14:13 +0900

    api gateway service 유레카서버 등록 및 first-service 랜덤포트 사용하여 로드밸런싱 적용

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,9 +1,14 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
@@ -11,5 +16,16 @@ public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to Second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header){
+        log.info(header);
+        return "Hello World in Second Service";
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Hi, there. This is a message from Second Service";
     }
 }
